b552bf20533feafa8e81cfcdcd5965d4e76429e1
Russ Panula <dfadev@dfa.net>
2018-12-06 12:53:20 +0000

    add missing dotfiles

---
+++ packages/carry-on-notify/.jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  verbose: true,
+  testPathIgnorePatterns: ["<rootDir>/build/", "<rootDir>/node_modules/", "<rootDir>/examples/", "<rootDir>/lib"],
+  collectCoverage: true,
+  coverageDirectory: "<rootDir>/coverage/"
+};
