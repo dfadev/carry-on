f4f72fdf1be28690acd78f43a515f57c698ae246
Russ Panula <dfadev@dfa.net>
2018-12-08 02:41:37 +0000

    add carry-on-react module

---
+++ packages/carry-on-react/src/index.js
@@ -0,0 +1,16 @@
+/** @format **/
+import { createContext } from "react";
+import makeStoreModule from "carry-on-store";
+import makeStateComponents from "./State";
+import makeStoreComponents from "./Store";
+
+const core = makeStoreModule(undefined, () => ({ Context: createContext({}) }));
+const state = makeStateComponents(core);
+const store = makeStoreComponents(core);
+
+export const deleteStore = core.deleteStore;
+export const register = core.register;
+export const State = state.State;
+export const withState = state.withState;
+export const Store = store.Store;
+export const withStore = store.withStore;
