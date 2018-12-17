57e4d11c85fef1ee9e83874f1f58a2f9807e47bf
Russ Panula <dfadev@dfa.net>
2018-12-17 20:44:09 +0000

    fix module not found errors

--- packages/carry-on-react/.jest.config.js
+++ packages/carry-on-react/.jest.config.js
@@ -4,5 +4,10 @@ module.exports = {
   verbose: true,
   testPathIgnorePatterns: ["<rootDir>/build/", "<rootDir>/node_modules/", "<rootDir>/examples/", "<rootDir>/lib", "<rootDir>/src/jest.setup.js"],
   collectCoverage: true,
-  coverageDirectory: "<rootDir>/coverage/"
+  coverageDirectory: "<rootDir>/coverage/",
+  moduleNameMapper: {
+    '^carry-on-notify$': '<rootDir>/node_modules/carry-on-notify/es/carry-on-notify.js',
+    '^carry-on-store$': '<rootDir>/node_modules/carry-on-store/es/carry-on-store.js',
+    '^carry-on-transaction$': '<rootDir>/node_modules/carry-on-transaction/es/carry-on-transaction.js'
+  }
 };
