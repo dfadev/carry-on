615b24506defd2922e65e18330ccee3ce7e137ce
Russ Panula <dfadev@dfa.net>
2018-12-19 05:02:50 +0000

    prettier

--- packages/carry-on-react/.jest.config.js
+++ packages/carry-on-react/.jest.config.js
@@ -1,13 +1,21 @@
 module.exports = {
-  //setupTestFrameworkScriptFile: "./node_modules/jest-enzyme/lib/index.js",
   setupTestFrameworkScriptFile: "./test/jest.setup.js",
   verbose: true,
-  testPathIgnorePatterns: ["<rootDir>/build/", "<rootDir>/node_modules/", "<rootDir>/examples/", "<rootDir>/lib", "<rootDir>/src/jest.setup.js"],
+  testPathIgnorePatterns: [
+    "<rootDir>/build/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/examples/",
+    "<rootDir>/lib",
+    "<rootDir>/src/jest.setup.js"
+  ],
   collectCoverage: true,
   coverageDirectory: "<rootDir>/coverage/",
   moduleNameMapper: {
-    '^carry-on-notify$': '<rootDir>/node_modules/carry-on-notify/es/carry-on-notify.js',
-    '^carry-on-store$': '<rootDir>/node_modules/carry-on-store/es/carry-on-store.js',
-    '^carry-on-transaction$': '<rootDir>/node_modules/carry-on-transaction/es/carry-on-transaction.js'
+    "^carry-on-notify$":
+      "<rootDir>/node_modules/carry-on-notify/es/carry-on-notify.js",
+    "^carry-on-store$":
+      "<rootDir>/node_modules/carry-on-store/es/carry-on-store.js",
+    "^carry-on-transaction$":
+      "<rootDir>/node_modules/carry-on-transaction/es/carry-on-transaction.js"
   }
 };
