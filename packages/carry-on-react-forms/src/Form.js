c8de0d4b28298b6a325800fc2f965a4854b366cd
Russ Panula <dfadev@dfa.net>
2018-12-23 23:19:06 +0000

    cleanup

--- packages/carry-on-react-forms/src/Form.js
+++ packages/carry-on-react-forms/src/Form.js
@@ -2,12 +2,7 @@
 import React from "react";
 import { State } from "carry-on-react";
 
-export default ({
-  store, // storeid
-  form = "form", // formid
-  children,
-  ...rest
-}) => (
+export default ({ store, form = "form", children, ...rest }) => (
   <State path={form} from={store} {...rest}>
     {state => (
       <form onSubmit={state.submit} onReset={state.reset}>
