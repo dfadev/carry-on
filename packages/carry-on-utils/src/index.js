57097f91dc8dc7b8c78f1c56ed3d19bf1adfb9e8
Russ Panula <dfadev@dfa.net>
2018-12-23 00:35:09 +0000

    remove deepmerge dependency

--- packages/carry-on-utils/src/index.js
+++ packages/carry-on-utils/src/index.js
@@ -2,7 +2,6 @@
 export { throttle, debounce } from "throttle-debounce";
 export { default as debouncePromise } from "debounce-promise";
 export { default as isEqual } from "fast-deep-equal";
-export { default as merge } from "deepmerge";
 export { merge as mutateMerge } from "micro-dash";
 
 export { default as isFunction } from "./isFunction";
