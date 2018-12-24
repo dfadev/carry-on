7834700542bd220e66d7aa597fc728ea8cba88bd
Russ Panula <dfadev@dfa.net>
2018-12-24 07:41:51 +0000

    remove micro-dash dependency

--- packages/carry-on-utils/src/mutateMerge.js
+++ packages/carry-on-utils/src/mutateMerge.js
@@ -1,4 +1,38 @@
-import { forEach, clone } from "micro-dash";
+function keys(object) {
+  return object ? Object.getOwnPropertyNames(object) : [];
+}
+
+function forOwn(object, iteratee) {
+  for (const key of keys(object)) {
+    if (iteratee(object[key], key) === false) {
+      break;
+    }
+  }
+  return object;
+}
+
+function forEach(collection, iteratee) {
+  if (Array.isArray(collection)) {
+    for (let i = 0, l = collection.length; i < l; ++i) {
+      if (iteratee(collection[i], i) === false) {
+        break;
+      }
+    }
+  } else {
+    forOwn(collection, iteratee);
+  }
+  return collection;
+}
+
+function clone(value) {
+  if (Array.isArray(value)) {
+    return value.slice();
+  }
+  if (value instanceof Object) {
+    return Object.assign({}, value);
+  }
+  return value;
+}
 
 export default function mutateMerge(object, ...sources) {
   for (const source of sources) {
