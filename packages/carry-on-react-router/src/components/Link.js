af553e321718339795fa06a6df8c409948c76791
Russ Panula <dfadev@dfa.net>
2019-01-05 12:21:05 +0000

    style

--- packages/carry-on-react-router/src/components/Link.js
+++ packages/carry-on-react-router/src/components/Link.js
@@ -2,26 +2,31 @@ import React from "react";
 import { carryOn } from "carry-on-react";
 import { getIn } from "carry-on-utils";
 
-const Link = carryOn({ id: "Link" }, (props, state) => {
-  const {
-    history = "app.history",
-    innerRef,
-    replace,
-    to,
-    onClick,
-    target,
-    ...rest
-  } = props;
-  const { handleClick, getHref } = getIn(state, history);
+const Link = carryOn(
+  { id: "Link" },
+  (
+    {
+      history = "app.history",
+      innerRef,
+      replace,
+      to,
+      onClick,
+      target,
+      ...rest
+    },
+    state
+  ) => {
+    const { handleClick, getHref } = getIn(state, history);
 
-  return (
-    <a
-      {...rest}
-      onClick={handleClick({ replace, to, onClick, target })}
-      href={getHref(to)}
-      ref={innerRef}
-    />
-  );
-});
+    return (
+      <a
+        {...rest}
+        onClick={handleClick({ replace, to, onClick, target })}
+        href={getHref(to)}
+        ref={innerRef}
+      />
+    );
+  }
+);
 
 export default Link;
--- packages/carry-on-react-router/src/components/Route.js
+++ packages/carry-on-react-router/src/components/Route.js
@@ -6,43 +6,45 @@ function isEmptyChildren(children) {
   return React.Children.count(children) === 0;
 }
 
-const Route = carryOn((props, state) => {
-  const {
-    history = "app.history",
-    location: loc,
-    computedMatch,
-    path,
-    children: childs,
-    component,
-    render
-  } = props;
+const Route = carryOn(
+  (
+    {
+      history = "app.history",
+      location: loc,
+      computedMatch,
+      path,
+      children: childs,
+      component,
+      render
+    },
+    state
+  ) => {
+    const hist = getIn(state, history);
+    const location = loc || hist.location;
 
-  const hist = getIn(state, history);
-  const location = loc || hist.location;
+    let match;
+    if (computedMatch) match = computedMatch;
+    else if (path) match = hist.matchPath(props);
+    //else match = hist.match; // ???
 
-  let match;
-  if (computedMatch) match = computedMatch;
-  else if (path) match = hist.matchPath(props);
-  //else match = hist.match; // ???
+    const renderProps = { location, match };
 
-  const renderProps = { location, match };
+    let children = childs;
+    if (Array.isArray(children) && children.length === 0) children = null;
 
-  let children = childs;
-  if (Array.isArray(children) && children.length === 0)
-    children = null;
+    if (typeof children === "function") {
+      children = children(renderProps);
+      if (children === undefined) children = null;
+    }
 
-  if (typeof children === "function") {
-    children = children(renderProps);
-    if (children === undefined) children = null;
+    if (children && !isEmptyChildren(children)) return children;
+    if (match)
+      if (component) return React.createElement(component, renderProps);
+      else if (render) return render(renderProps);
+
+    return null;
   }
-
-  if (children && !isEmptyChildren(children)) return children;
-  if (match)
-    if (component) return React.createElement(component, renderProps);
-    else if (render) return render(renderProps);
-
-  return null;
-});
+);
 
 Route.displayName = "Route";
 
