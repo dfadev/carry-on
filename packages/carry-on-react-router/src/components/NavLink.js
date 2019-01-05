681203bc7487f1da2b805ba7dd8cf9133773e38b
Russ Panula <dfadev@dfa.net>
2019-01-05 12:20:51 +0000

    reduce to one state component

--- packages/carry-on-react-router/src/components/NavLink.js
+++ packages/carry-on-react-router/src/components/NavLink.js
@@ -1,75 +1,65 @@
 import React from "react";
-import Route from "./Route";
-
-import Link from "./Link";
+import { carryOn } from "carry-on-react";
+import { getIn } from "carry-on-utils";
 
 function joinClassnames(...classnames) {
   return classnames.filter(i => i).join(" ");
 }
 
-/**
- * A <Link> wrapper that knows if it's "active" or not.
- */
-function NavLink({
-  debug,
-  verbose,
-  from,
-  history,
-  "aria-current": ariaCurrent = "page",
-  activeClassName = "active",
-  activeStyle,
-  className: classNameProp,
-  exact,
-  isActive: isActiveProp,
-  location: loc,
-  strict,
-  style: styleProp,
-  to,
-  ...rest
-}) {
-  const path = typeof to === "object" ? to.pathname : to;
+const escapedRE = /([.+*?=^!:${}()[\]|/\\])/g;
 
-  // Regex taken from: https://github.com/pillarjs/path-to-regexp/blob/master/index.js#L202
-  const escapedPath = path && path.replace(/([.+*?=^!:${}()[\]|/\\])/g, "\\$1");
+const NavLink = carryOn(
+  (
+    {
+      history = "app.history",
+      "aria-current": ariaCurrent = "page",
+      activeClassName = "active",
+      activeStyle,
+      className: classNameProp,
+      exact,
+      isActive: isActiveProp,
+      location: loc,
+      style: styleProp,
+      strict,
+      sensitive,
+      to,
+      replace,
+      onClick,
+      target,
+      innerRef,
+      ...rest
+    },
+    state
+  ) => {
+    const hist = getIn(state, history),
+      location = loc || hist.location,
+      path = typeof to === "object" ? to.pathname : to,
+      escapedPath = path && path.replace(escapedRE, "\\$1"),
+      match = hist.matchPath({
+        path: escapedPath,
+        exact,
+        strict,
+        sensitive
+      }),
+      isActive = !!(isActiveProp ? isActiveProp(match, location) : match),
+      className = isActive
+        ? joinClassnames(classNameProp, activeClassName)
+        : classNameProp,
+      style = isActive ? { ...styleProp, ...activeStyle } : styleProp,
+      { handleClick, getHref } = hist;
 
-  return (
-    <Route
-      id="NavLink"
-      from={from}
-      debug={debug}
-      verbose={verbose}
-      history={history}
-      path={escapedPath}
-      exact={exact}
-      strict={strict}
-      location={loc}
-    >
-      {({ location, match }) => {
-        const isActive = !!(isActiveProp
-          ? isActiveProp(match, location)
-          : match);
-
-        const className = isActive
-          ? joinClassnames(classNameProp, activeClassName)
-          : classNameProp;
-        const style = isActive ? { ...styleProp, ...activeStyle } : styleProp;
-
-        return (
-          <Link
-            from={from}
-            debug={debug}
-            verbose={verbose}
-            history={history}
-            aria-current={(isActive && ariaCurrent) || null}
-            className={className}
-            style={style}
-            to={to}
-            {...rest}
-          />
-        );
-      }}
-    </Route>
-  );
-}
+    return (
+      <a
+        {...rest}
+        onClick={handleClick({ replace, to, onClick, target })}
+        href={getHref(to)}
+        ref={innerRef}
+        style={style}
+        className={className}
+        aria-current={(isActive && ariaCurrent) || null}
+      />
+    );
+  }
+);
 
 export default NavLink;
