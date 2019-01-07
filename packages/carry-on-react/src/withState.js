9c7cf91dbff610e4def6fd916f61ae3cb4543512
Russ Panula <dfadev@dfa.net>
2019-01-07 22:45:33 +0000

    asProp is stateProp not compProp

--- packages/carry-on-react/src/withState.js
+++ packages/carry-on-react/src/withState.js
@@ -5,28 +5,27 @@ import State from "./State";
 
 // a HOC that wraps the State component
 export default (stateProps = {}) => WrappedComponent => {
-  const WithState = props => {
-    const { asProp, ...compProps } = props;
+  const WithState = props => (
     // spreading state here will cause the watch index to mark all fields as accessed
-    return (
-      <State {...stateProps}>
-        {state => {
-          if (asProp)
-            return <WrappedComponent {...compProps} {...{ [asProp]: state }} />;
-
-          // fix String/Number/Boolean instanceof
-          const val = state && state.valueOf();
-          const type = typeof val;
-          // spread object props, or deliver as "state" prop if it's not an object
-          return type === "object" && !Array.isArray(state) ? (
-            <WrappedComponent {...props} {...state} />
-          ) : (
-            <WrappedComponent {...props} state={state} />
+    <State {...stateProps}>
+      {state => {
+        if (stateProps.asProp)
+          return (
+            <WrappedComponent {...props} {...{ [stateProps.asProp]: state }} />
           );
-        }}
-      </State>
-    );
-  };
+
+        // fix String/Number/Boolean instanceof
+        const val = state && state.valueOf();
+        const type = typeof val;
+        // spread object props, or deliver as "state" prop if it's not an object
+        return type === "object" && !Array.isArray(state) ? (
+          <WrappedComponent {...props} {...state} />
+        ) : (
+          <WrappedComponent {...props} state={state} />
+        );
+      }}
+    </State>
+  );
 
   WithState.displayName = `withState(${WrappedComponent.displayName ||
     WrappedComponent.name})`;
