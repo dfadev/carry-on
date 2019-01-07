1b7352b2fb78f645ea7e60f18f7480cb8928d31f
Russ Panula <dfadev@dfa.net>
2019-01-07 16:32:13 +0000

    update tests

--- packages/carry-on-store/test/devtools.test.js
+++ packages/carry-on-store/test/devtools.test.js
@@ -11,30 +11,29 @@ test("new devTools with opts", () => {
 test("subscribe is called", () => {
   let subscribeCalled = 0;
   let msgFn;
-  let subscribe = (fn) => {
+  let subscribe = fn => {
     subscribeCalled++;
     msgFn = fn;
-  }
+  };
 
   window.__REDUX_DEVTOOLS_EXTENSION__ = {
-    connect: (opts) => ({
+    connect: opts => ({
       subscribe,
-      send: (action, state) => {
-      }
+      send: (action, state) => {}
     })
   };
 
   const plugin = new devTools();
   let setCount = 0;
   let set = (action, ...args) => {
-    action && action();
+    action && action({});
     setCount++;
     return { id: 1 };
   };
   set = plugin.middleware({ isNested: () => false, set, next: set });
 
   set();
-  set(() => { });
+  set(() => {});
 
   expect(subscribeCalled).toBe(1);
   expect(setCount).toBe(2);
@@ -48,20 +47,18 @@ test("subscribe is called", () => {
   expect(setCount).toBe(3);
 });
 
-
 test("disable timetravel works", () => {
   let subscribeCalled = 0;
   let msgFn;
   let subscribe = fn => {
     subscribeCalled++;
     msgFn = fn;
-  }
+  };
 
   window.__REDUX_DEVTOOLS_EXTENSION__ = {
-    connect: (opts) => ({
+    connect: opts => ({
       subscribe,
-      send: (action, state) => {
-      }
+      send: (action, state) => {}
     })
   };
 
@@ -74,20 +71,133 @@ test("disable timetravel works", () => {
   };
   set = plugin.middleware({ isNested: () => false, set, next: set });
 
-  set();
-  set(() => { }, "Time Travel");
-  set();
+  set(() => {});
+  set(() => {}, "Time Travel");
+  set(() => {});
 
   expect(subscribeCalled).toBe(0);
   expect(setCount).toBe(3);
 
-  msgFn && msgFn({ type: "DISPATCH", payload: { type: "JUMP_TO_STATE", index: 0 } });
+  msgFn &&
+    msgFn({ type: "DISPATCH", payload: { type: "JUMP_TO_STATE", index: 0 } });
 
   expect(setCount).toBe(3);
 
-  msgFn && msgFn({ type: "OTHER", payload: { type: "JUMP_TO_STATE", index: 0 } });
+  msgFn &&
+    msgFn({ type: "OTHER", payload: { type: "JUMP_TO_STATE", index: 0 } });
 
   expect(setCount).toBe(3);
 });
 
+test("dispose", () => {
+  let subscribeCalled = 0;
+  let unsubscribeCalled = 0;
+  let msgFn;
+  let subscribe = fn => {
+    subscribeCalled++;
+    msgFn = fn;
+    return () => {
+      unsubscribeCalled++;
+    };
+  };
 
+  window.__REDUX_DEVTOOLS_EXTENSION__ = {
+    connect: opts => ({
+      subscribe,
+      send: (action, state) => {}
+    })
+  };
+
+  const plugin = new devTools();
+  let setCount = 0;
+  let set = (action, ...args) => {
+    action && action({});
+    setCount++;
+    return { id: 1 };
+  };
+  set = plugin.middleware({ isNested: () => false, set, next: set });
+
+  set(() => {});
+
+  expect(subscribeCalled).toBe(1);
+
+  plugin.dispose();
+});
+
+test("nested", () => {
+  let subscribeCalled = 0;
+  let msgFn;
+  let subscribe = fn => {
+    subscribeCalled++;
+    msgFn = fn;
+  };
+
+  window.__REDUX_DEVTOOLS_EXTENSION__ = {
+    connect: opts => ({
+      subscribe,
+      send: (action, state) => {}
+    })
+  };
+
+  const plugin = new devTools({ timeTravel: true });
+  let setCount = 0;
+  let set = (action, ...args) => {
+    action && action();
+    setCount++;
+    return { id: 1 };
+  };
+
+  set = plugin.middleware({ isNested: () => true, set, next: set });
+
+  set(() => {});
+  set(() => {}, "Time Travel");
+  set(() => {});
+
+  expect(subscribeCalled).toBe(2);
+  expect(setCount).toBe(3);
+
+  msgFn &&
+    msgFn({ type: "DISPATCH", payload: { type: "JUMP_TO_STATE", index: 0 } });
+
+  expect(setCount).toBe(4);
+
+  msgFn &&
+    msgFn({ type: "OTHER", payload: { type: "JUMP_TO_STATE", index: 0 } });
+
+  expect(setCount).toBe(4);
+});
+
+test("tt", () => {
+  let subscribeCalled = 0;
+  let msgFn;
+  let subscribe = fn => {
+    subscribeCalled++;
+    msgFn = fn;
+  };
+
+  window.__REDUX_DEVTOOLS_EXTENSION__ = {
+    connect: opts => ({
+      subscribe,
+      send: (action, state) => {}
+    })
+  };
+
+  const plugin = new devTools({ timeTravel: true });
+  let setCount = 0;
+  let state = { };
+  let set = (action, ...args) => {
+    setCount++;
+    state = action && action(state);
+    return state;
+  };
+
+  set = plugin.middleware({ isNested: () => false, set, next: set });
+
+  set(state => ({ field2: "value", field3: "value" }));
+  set(state => ({ field3: "value", none: "field" }));
+
+  msgFn &&
+    msgFn({ type: "DISPATCH", payload: { type: "JUMP_TO_STATE", index: 0 } });
+
+  expect(state).toMatchSnapshot();
+});
