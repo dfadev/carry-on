ce017cd26e7af49099c6f80068663dad70c2274f
Russ Panula <russ@dfa.net>
2019-01-13 03:46:23 +0000

    use Context to pass match to children

--- packages/carry-on-react-router/src/components/Route.js
+++ packages/carry-on-react-router/src/components/Route.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { carryOn } from "carry-on-react";
 import { getIn } from "carry-on-utils";
+import RouterContext from "./RouterContext";
 
 function isEmptyChildren(children) {
   return React.Children.count(children) === 0;
@@ -28,26 +29,43 @@ const Route = carryOn(
 
     let match;
     if (computedMatch) match = computedMatch;
-    else if (path) match = hist.matchPath({ path, exact, strict, sensitive });
-    else match = hist.match;
+    else if (path)
+      match = hist.matchPath({ path, exact, strict, sensitive });
+    else 
+      match = hist.match;
 
-    const renderProps = { location, match, history: hist };
-    if (hist.staticContext) renderProps.staticContext = hist.staticContext;
+    return (
+      <RouterContext.Consumer>
+        {(context = {}) => {
+          if (context.match !== undefined && !path) match = context.match;
 
-    let children = childs;
-    if (Array.isArray(children) && children.length === 0) children = null;
+          const renderProps = { ...context, location, match, history: hist };
+          if (hist.staticContext)
+            renderProps.staticContext = hist.staticContext;
 
-    if (typeof children === "function") {
-      children = children(renderProps);
-      if (children === undefined) children = null;
-    }
+          let children = childs;
+          if (Array.isArray(children) && children.length === 0) children = null;
+          else if (typeof children === "function") {
+            children = children(renderProps);
+            if (children === undefined) children = null;
+          }
 
-    if (children && !isEmptyChildren(children)) return children;
-    if (match)
-      if (component) return React.createElement(component, renderProps);
-      else if (render) return render(renderProps);
+          let content = null;
+          if (children && !isEmptyChildren(children)) content = children;
+          else if (match) {
+            if (component)
+              content = React.createElement(component, renderProps);
+            else if (render) content = render(renderProps);
+          }
 
-    return null;
+          return (
+            <RouterContext.Provider value={renderProps}>
+              {content}
+            </RouterContext.Provider>
+          );
+        }}
+      </RouterContext.Consumer>
+    );
   }
 );
 
