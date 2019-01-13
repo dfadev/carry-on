728743805f6238ed301b770ab1f2bf399dd50a97
Russ Panula <russ@dfa.net>
2019-01-13 22:09:45 +0000

    - add FormContext to pass store and form parameters to child components - Form component now registers form plugin and state specified by the register prop

--- packages/carry-on-react-forms/src/Form.js
+++ packages/carry-on-react-forms/src/Form.js
@@ -1,13 +1,21 @@
 /** @format **/
 import React from "react";
 import { State } from "carry-on-react";
+import FormContext from "./FormContext";
+import plugin from "./plugin";
 
-export default ({ store, form = "form", children, ...rest }) => (
-  <State path={form} from={store} {...rest}>
-    {state => (
-      <form onSubmit={state.submit} onReset={state.reset}>
-        {children}
-      </form>
-    )}
-  </State>
+export default ({ store, form = "form", children, register = [], ...rest }) => (
+  <FormContext.Provider value={{ store, form }}>
+    <State
+      path={form}
+      from={store}
+      register={[].concat(register).concat(plugin({ id: form, ...rest }))}
+    >
+      {(state = {}) => (
+        <form onSubmit={state.submit} onReset={state.reset}>
+          {children}
+        </form>
+      )}
+    </State>
+  </FormContext.Provider>
 );
