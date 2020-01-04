5dc1de5ef3f312dbdcd9bdca01163ed586bd02ac
Russ Panula <russ@dfa.net>
2020-01-04 10:56:09 +0000

    add mutateMergeStrip variation that skips merging functions

--- packages/carry-on-utils/src/mutateMerge.js
+++ packages/carry-on-utils/src/mutateMerge.js
@@ -18,3 +18,27 @@ export default function mutateMerge(object, ...sources) {
   }
   return object;
 }
+
+export function mutateMergeStrip(object, ...sources) {
+  for (const source of sources) {
+    forEach(source, (value, key) => {
+      const myValue = object[key];
+      const myValueType = typeof myValue;
+      const sourceValueType = typeof value;
+      // original micro-dash tries to clone functions
+      if (myValue instanceof Object && myValueType !== "function") {
+        value = mutateMergeStrip(clone(myValue), value);
+      }
+      // assign undefined to maybe sparse array
+      if (value === undefined && Array.isArray(object)) object[key] = value;
+      // don't assign if we don't need to, assign undefined to maybe sparse array
+      else if (
+        object[key] !== value &&
+        myValueType !== "function" &&
+        sourceValueType !== "function"
+      )
+        object[key] = value;
+    });
+  }
+  return object;
+}
