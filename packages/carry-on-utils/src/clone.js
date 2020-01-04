9f7bed67e47036b18a7769edf805d493d444c657
Russ Panula <russ@dfa.net>
2020-01-04 06:32:56 +0000

    dont bother cloning number/string types

--- packages/carry-on-utils/src/clone.js
+++ packages/carry-on-utils/src/clone.js
@@ -1,7 +1,10 @@
 /** @format **/
 export default function clone(value) {
   if (Array.isArray(value)) return value.slice();
+  const type = typeof value;
 
+  if (type === "number") return value;
+  if (type === "string") return value;
   if (value instanceof Object) return { ...value };
 
   return value;
