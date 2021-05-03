78be5e4e73959f8ea761876fcf585b126bb0dccc
Russ Panula <russ@dfa.net>
2021-05-03 00:59:14 -0400

    explicit exports

--- packages/carry-on-store/src/index.js
+++ packages/carry-on-store/src/index.js
@@ -3,9 +3,18 @@ import { enablePatches } from "immer";
 
 enablePatches();
 
-export * from "./store";
-export * from "./changeTracking";
-export * from "./watch";
+export {
+  deleteStore,
+  initStores,
+  getStore,
+  register,
+  connect,
+  subscribe,
+  watchGet,
+  initMessageType
+} from "./store";
+export { compareChanges, calculateChanges } from "./changeTracking";
+export { watch, Watch } from "./watch";
 export { default as notify } from "./notify";
 export { default as devTools } from "./devtools";
 export { default as transaction } from "./transaction";
