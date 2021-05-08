1634fe7b2b1aae6fcd4d7f69a907bbab6f0073c1
Russ Panula <russ@dfa.net>
2021-05-07 20:24:45 -0400

    simplify buildHelper

--- buildHelper.js
+++ buildHelper.js
@@ -3,61 +3,30 @@ import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
 import eslint from "@rollup/plugin-eslint";
 
-export default function buildHelper(pkg, globals) {
-  const input = pkg.source;
-
-  const external = Object.keys(pkg.dependencies || {})
-    .concat(Object.keys(pkg.peerDependencies || {}))
-    .concat([
-      "@babel/runtime/helpers/extends",
-      "@babel/runtime/helpers/defineProperty"
-    ]);
-
-  const watch = {
-    clearScreen: false,
-    chokidar: {
-      usePolling: true
-    }
-  };
-
-  return [
-    {
-      input,
-      output: {
+export default pkg => [
+  {
+    input: pkg.source,
+    output: [
+      {
         name: pkg.name,
         file: pkg.browser,
         format: "umd",
-        sourcemap: true,
-        globals
+        sourcemap: true
       },
-      plugins: [
-        resolve(),
-        eslint(),
-        babel({
-          rootMode: "upward",
-          exclude: ["node_modules/**"],
-        }),
-        commonjs()
-      ],
-      watch,
-      external
-    },
-
-    {
-      input,
-      output: [
-        { file: pkg.main, format: "cjs", sourcemap: true },
-        { file: pkg.module, format: "es", sourcemap: true }
-      ],
-      plugins: [
-        eslint(),
-        babel({
-          rootMode: "upward",
-          exclude: ["node_modules/**"],
-        })
-      ],
-      watch,
-      external
-    }
-  ];
-}
+      { file: pkg.main, format: "cjs", sourcemap: true },
+      { file: pkg.module, format: "es", sourcemap: true }
+    ],
+    plugins: [
+      resolve(),
+      eslint(),
+      babel({
+        rootMode: "upward",
+        exclude: ["node_modules/**"],
+        babelHelpers: "runtime"
+      }),
+      commonjs()
+    ],
+    watch: { clearScreen: false },
+    external: [...Object.keys(pkg.peerDependencies || {}), /@babel\/runtime/]
+  }
+];
