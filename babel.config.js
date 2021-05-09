123ff45b8af2d539e39e05634cc723a36c07b633
Russ Panula <russ@dfa.net>
2021-05-09 07:01:31 -0400

    move babel config back, simplify

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,7 @@
-module.exports = require("./package.json").babel;
+module.exports = {
+  presets: [
+    ["@babel/preset-env", { targets: { browsers: ["> 3%, not dead"] } }],
+    "@babel/preset-react"
+  ],
+  plugins: [["@babel/plugin-transform-runtime", { corejs: 3 }]]
+};
