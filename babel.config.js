f95206197eb297a58a7d6f8a40d61eadb767caac
Russ Panula <russ@dfa.net>
2021-05-10 06:16:55 -0400

    simplify

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  presets: [
-    ["@babel/preset-env", { targets: { browsers: ["> 3%, not dead"] } }],
-    "@babel/preset-react"
-  ],
+  presets: ["@babel/preset-env", "@babel/preset-react"],
   plugins: [["@babel/plugin-transform-runtime", { corejs: 3 }]]
 };
