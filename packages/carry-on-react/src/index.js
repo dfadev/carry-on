8ebb7f7829af255b0ea2b28413a0257a2702ed88
Russ Panula <russ@dfa.net>
2021-05-14 20:58:25 -0400

    export Store component

--- packages/carry-on-react/src/index.js
+++ packages/carry-on-react/src/index.js
@@ -3,3 +3,4 @@ export { default as withState } from "./withState";
 export { default as carryOn } from "./carryOn";
 export { default as DevTools } from "./DevTools";
 export { default as useCarryOn } from "./useCarryOn";
+export { default as Store } from "./Store";
