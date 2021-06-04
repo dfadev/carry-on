f16c8bd897e071011bd5f8f24062c4d1823069fe
Russ Panula <russ@dfa.net>
2021-06-04 02:39:49 -0400

    add more react-forms tests

--- packages/carry-on-react-forms/test/Field.test.js
+++ packages/carry-on-react-forms/test/Field.test.js
@@ -59,3 +59,168 @@ it("should render a simple input field in named form", () => {
   expect(asFragment()).toMatchSnapshot("render match");
   expect(get()).toMatchSnapshot("store match");
 });
+
+it("should handle onChange", () => {
+  let onChange;
+  const { asFragment } = render(
+    <Form id="theform" register={formState("theform")}>
+      <Field path="field1">
+        {state => {
+          onChange = state.element.onChange;
+          return <input {...state.element} />;
+        }}
+      </Field>
+    </Form>
+  );
+
+  onChange({
+    target: {
+      type: "number",
+      value: 0
+    }
+  });
+
+  expect(asFragment()).toMatchSnapshot();
+
+  onChange({
+    target: {
+      type: "number",
+      value: NaN
+    }
+  });
+
+  expect(asFragment()).toMatchSnapshot();
+
+  onChange({
+    target: {
+      type: "checkbox",
+      checked: true
+    }
+  });
+
+  expect(asFragment()).toMatchSnapshot();
+
+  onChange({
+    target: {
+      value: ""
+    }
+  });
+
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("should handle onFocus", () => {
+  let onFocus;
+  const { asFragment } = render(
+    <Form id="theform" register={formState("theform")}>
+      <Field path="field1">
+        {state => {
+          onFocus = state.element.onFocus;
+          return <input {...state.element} />;
+        }}
+      </Field>
+    </Form>
+  );
+
+  onFocus();
+  expect(get().theform.visited.field1).toBe(true);
+});
+
+it("should handle onBlur", () => {
+  let onBlur;
+  const { asFragment } = render(
+    <Form id="theform" register={formState("theform")}>
+      <Field path="field1">
+        {state => {
+          onBlur = state.element.onBlur;
+          return <input {...state.element} />;
+        }}
+      </Field>
+    </Form>
+  );
+
+  onBlur();
+  expect(get().theform.touched.field1).toBe(true);
+});
+
+
+it("should handle setValue", () => {
+  let setValue;
+  const { asFragment } = render(
+    <Form id="theform" register={formState("theform")}>
+      <Field path="field1">
+        {state => {
+          setValue = state.setValue;
+          return <input {...state.element} />;
+        }}
+      </Field>
+    </Form>
+  );
+
+  setValue("123");
+  expect(get().theform.values.field1).toBe("123");
+});
+
+it("should handle setVisited", () => {
+  let setVisited;
+  const { asFragment } = render(
+    <Form id="theform" register={formState("theform")}>
+      <Field path="field1">
+        {state => {
+          setVisited = state.setVisited;
+          return <input {...state.element} />;
+        }}
+      </Field>
+    </Form>
+  );
+
+  setVisited(true);
+  expect(get().theform.visited.field1).toBe(true);
+});
+
+it("should handle setTouched", () => {
+  let setTouched;
+  const { asFragment } = render(
+    <Form id="theform" register={formState("theform")}>
+      <Field path="field1">
+        {state => {
+          setTouched = state.setTouched;
+          return <input {...state.element} />;
+        }}
+      </Field>
+    </Form>
+  );
+
+  setTouched(true);
+  expect(get().theform.touched.field1).toBe(true);
+});
+
+it("should handle setError", () => {
+  let setError;
+  const { asFragment } = render(
+    <Form id="theform" register={formState("theform")}>
+      <Field path="field1">
+        {state => {
+          setError = state.setError;
+          return <input {...state.element} />;
+        }}
+      </Field>
+    </Form>
+  );
+
+  setError(true);
+  expect(get().theform.errors.field1).toBe(true);
+});
+
+it("should handle radio", () => {
+  const { asFragment } = render(
+    <Form id="theform" register={formState("theform")}>
+      <Field path="checkbox" type="checkbox">
+        {state => {
+          expect(state.element.checked).toBe(false);
+          return <input {...state.element} />;
+        }}
+      </Field>
+    </Form>
+  );
+});
--- packages/carry-on-react-forms/test/Form.test.js
+++ packages/carry-on-react-forms/test/Form.test.js
@@ -2,9 +2,432 @@
  * @jest-environment jsdom
  */
 import React from "react";
+import { initStores, register, connect, get, set } from "carry-on-store";
+import { Store } from "carry-on-react";
 import { render } from "@testing-library/react";
 import Form from "../src/Form";
+import Field from "../src/Field";
+import plugin from "../src/plugin";
+
+beforeEach(() => initStores());
 
 it("should render <Form>", () => {
   expect(render(<Form />).asFragment()).toMatchSnapshot();
+});
+
+const formState = (formId = "form", opts) => ({
+  state: ({ get, set }) => ({
+    [formId]: {
+      initialValues: {
+        field1: "value1",
+        field2: "value2",
+        checkbox: false
+      },
+      ...opts
+    }
+  })
+});
+
+it("should validate", async () => {
+  let validateCalled = 0;
+  const { asFragment } = render(
+    <Form
+      register={formState()}
+      onValidate={() => () => {
+        validateCalled += 1;
+      }}
+    >
+      <Field path="field1">
+        {state => {
+          expect(state).toMatchSnapshot("prop match");
+          return <input {...state.element} />;
+        }}
+      </Field>
+    </Form>
+  );
+
+  expect(asFragment()).toMatchSnapshot("render match");
+  expect(get()).toMatchSnapshot("store match");
+
+  get().form.setFieldValue("field1", "123");
+  await new Promise(r => setTimeout(r, 201));
+
+  expect(validateCalled).toBe(1);
+
+  get().form.setFieldValue("field1", "345");
+
+  await new Promise(r => setTimeout(r, 201));
+  expect(validateCalled).toBe(2);
+});
+
+it("should handle hasError", () => {
+  const { asFragment } = render(
+    <Form register={formState()}>
+      <Field path="field1">{state => <input {...state.element} />}</Field>
+    </Form>
+  );
+
+  expect(get().form.hasError("field1")).toBe(false);
+});
+
+it("should handle setValues", () => {
+  const { asFragment } = render(
+    <Form register={formState()}>
+      <Field path="field1">{state => <input {...state.element} />}</Field>
+    </Form>
+  );
+
+  get().form.setValues({ field1: "zzz" });
+
+  expect(get().form.values).toStrictEqual({ field1: "zzz" });
+});
+
+it("should handle setInitialValues", () => {
+  const { asFragment } = render(
+    <Form register={formState()}>
+      <Field path="field1" default="abc">
+        {state => <input {...state.element} />}
+      </Field>
+    </Form>
+  );
+
+  get().form.setInitialValues({ thing: "1" });
+
+  expect(get().form.values).toStrictEqual({ thing: "1" });
+});
+
+it("should handle setInitialValues specified as a function", () => {
+  const { asFragment } = render(
+    <Form register={formState()}>
+      <Field path="field1" default="abc">
+        {state => <input {...state.element} />}
+      </Field>
+    </Form>
+  );
+
+  get().form.setInitialValues(() => ({ thing: "1" }));
+
+  expect(get().form.values).toStrictEqual({ thing: "1" });
+});
+
+it("should handle setErrors", () => {
+  const { asFragment } = render(
+    <Form register={formState()}>
+      <Field path="field1" default="abc">
+        {state => <input {...state.element} />}
+      </Field>
+    </Form>
+  );
+
+  get().form.setErrors({});
+
+  expect(get().form.errors).toStrictEqual({});
+
+  get().form.setErrors({ merge: false });
+  expect(get().form.errors).toStrictEqual(undefined);
+});
+
+it("should handle setTouched", () => {
+  const { asFragment } = render(
+    <Form register={formState()}>
+      <Field path="field1" default="abc">
+        {state => <input {...state.element} />}
+      </Field>
+    </Form>
+  );
+
+  get().form.setTouched({});
+
+  expect(get().form.touched).toStrictEqual({});
+
+  get().form.setTouched({}, false);
+  expect(get().form.touched).toStrictEqual({});
+});
+
+it("should handle reset specified as prop", () => {
+  let onResetCalled = 0;
+  const { asFragment } = render(
+    <Form
+      register={formState()}
+      onReset={() => () => {
+        onResetCalled += 1;
+      }}
+    >
+      <Field path="field1" default="abc">
+        {state => <input {...state.element} />}
+      </Field>
+    </Form>
+  );
+
+  let preventDefaultCalled = 0;
+  get().form.reset({
+    preventDefault: () => {
+      preventDefaultCalled += 1;
+    }
+  });
+
+  expect(onResetCalled).toBe(1);
+  expect(preventDefaultCalled).toBe(1);
+});
+
+it("should handle reset stored in state", () => {
+  let onResetCalled = 0;
+
+  const { asFragment } = render(
+    <Form
+      register={formState("form", {
+        onReset: () => {
+          onResetCalled += 1;
+        }
+      })}
+    >
+      <Field path="field1" default="abc">
+        {state => <input {...state.element} />}
+      </Field>
+    </Form>
+  );
+
+  get().form.reset();
+
+  expect(onResetCalled).toBe(1);
+});
+
+it("should handle reset with no onReset", () => {
+  const { asFragment } = render(
+    <Form register={formState("form")}>
+      <Field path="field1" default="abc">
+        {state => <input {...state.element} />}
+      </Field>
+    </Form>
+  );
+
+  get().form.setFieldValue("field1", "zzz");
+  get().form.reset();
+  expect(get().form.values).toMatchSnapshot();
+});
+
+it("should handle validate onValidate stored in state", async () => {
+  let onValidateCalled = 0;
+
+  register(
+    formState("form", {
+      onValidate: () => {
+        onValidateCalled += 1;
+      }
+    })
+  );
+
+  const { asFragment } = render(
+    <Form>
+      <Field path="field1" default="abc">
+        {state => <input {...state.element} />}
+      </Field>
+    </Form>
+  );
+
+  get().form.setFieldValue("field1", "zzz");
+  await new Promise(r => setTimeout(r, 201));
+  expect(onValidateCalled).toBe(1);
+});
+
+it("should handle submit specified as prop", async () => {
+  let onSubmitCalled = 0;
+
+  const { asFragment } = render(
+    <Form
+      register={formState()}
+      onSubmit={() => () => {
+        onSubmitCalled += 1;
+        return true;
+      }}
+    >
+      <Field path="field1" default="abc">
+        {state => <input {...state.element} />}
+      </Field>
+    </Form>
+  );
+
+  get().form.setFieldValue("field1", "zzz");
+  await new Promise(r => setTimeout(r, 1));
+  let preventDefaultCalled = 0;
+  get().form.submit({
+    preventDefault: () => {
+      preventDefaultCalled += 1;
+    }
+  });
+  await new Promise(r => setTimeout(r, 1));
+  expect(onSubmitCalled).toBe(1);
+  expect(preventDefaultCalled).toBe(1);
+});
+
+it("should handle submit stored as state", () => {
+  let onSubmitCalled = 0;
+
+  const { asFragment } = render(
+    <Form
+      register={formState("form", {
+        onSubmit: () => {
+          onSubmitCalled += 1;
+        }
+      })}
+    >
+      <Field path="field1" default="abc">
+        {state => <input {...state.element} />}
+      </Field>
+    </Form>
+  );
+
+  get().form.submit();
+  expect(onSubmitCalled).toBe(1);
+});
+
+it("should handle no onSubmit specified", async () => {
+  const { asFragment } = render(
+    <Form>
+      <Field path="field1" default="abc">
+        {state => <input {...state.element} />}
+      </Field>
+    </Form>
+  );
+
+  get().form.submit();
+  await new Promise(r => setTimeout(r, 1));
+  expect(get().form.isSubmitting).toBe(false);
+});
+
+it("should ignore submit when validating", async () => {
+  let onSubmitCalled = 0;
+  let onValidateCalled = 0;
+
+  const { asFragment } = render(
+    <Form
+      register={formState()}
+      onSubmit={() => () => {
+        onSubmitCalled += 1;
+      }}
+      onValidate={() => () => {
+        onValidateCalled += 1;
+      }}
+    >
+      <Field path="field1" default="abc">
+        {state => <input {...state.element} />}
+      </Field>
+    </Form>
+  );
+
+  get().form.setFieldValue("field1", "def");
+  get().form.submit();
+  await new Promise(r => setTimeout(r, 201));
+
+  expect(onSubmitCalled).toBe(0);
+  expect(onValidateCalled).toBe(1);
+});
+
+it("should reset errors/visited/touched/isPristine after submit", async () => {
+  let onSubmitCalled = 0;
+
+  const { asFragment } = render(
+    <Form
+      register={formState()}
+      onSubmit={() => () => {
+        onSubmitCalled += 1;
+        return true;
+      }}
+    >
+      <Field path="field1" default="abc">
+        {state => <input {...state.element} />}
+      </Field>
+    </Form>
+  );
+
+  get().form.setFieldTouched("field1", true);
+  get().form.setFieldVisited("field1", true);
+  get().form.setFieldError("field1", "abc");
+  get().form.submit();
+  await new Promise(r => setTimeout(r, 500));
+
+  expect(onSubmitCalled).toBe(1);
+
+  expect(get().form.isPristine).toBe(true);
+  expect(get().form.errors).toStrictEqual({});
+  expect(get().form.visited).toStrictEqual({});
+  expect(get().form.touched).toStrictEqual({});
+});
+
+it("should gracefully handle exception in submit", async () => {
+  let onSubmitCalled = 0;
+  let errorThrown = 0;
+
+  const { asFragment } = render(
+    <Form
+      register={formState()}
+      onSubmit={() => () => {
+        onSubmitCalled += 1;
+        return new Promise(() => {
+          errorThrown += 1;
+          throw new Error("test error");
+        });
+      }}
+    >
+      <Field path="field1" default="abc">
+        {state => <input {...state.element} />}
+      </Field>
+    </Form>
+  );
+
+  get().form.submit();
+  await new Promise(r => setTimeout(r, 500));
+
+  expect(onSubmitCalled).toBe(1);
+  expect(errorThrown).toBe(1);
+  expect(get().form.isSubmitting).toBe(false);
+});
+
+it("renders store id", () => {
+  expect(
+    render(
+      <Store id="zzz">
+        <Form id="123" />
+      </Store>
+    ).asFragment()
+  ).toMatchSnapshot();
+});
+
+it("handles initialValues specified as prop", () => {
+  const { asFragment } = render(<Form initialValues={{ some: "stuff" }} />);
+
+  expect(get()).toMatchSnapshot();
+});
+
+it("handles initialValues specified as a function", () => {
+  const { asFragment } = render(
+    <Form initialValues={() => ({ some: "stuff" })} />
+  );
+
+  expect(get()).toMatchSnapshot();
+});
+
+it("handles submit with no validate", () => {
+  let onSubmitCalled = 0;
+  const { asFragment } = render(
+    <Form
+      onSubmit={() => {
+        onSubmitCalled += 1;
+      }}
+    />
+  );
+  get().form.submit();
+  expect(onSubmitCalled).toBe(1);
+});
+
+it("should handle setValues using same values", () => {
+  const { asFragment } = render(
+    <Form register={formState()}>
+      <Field path="field1">{state => <input {...state.element} />}</Field>
+    </Form>
+  );
+
+  get().form.setValues(get().form.values);
+
+  expect(get().form.values).toMatchSnapshot();
 });
