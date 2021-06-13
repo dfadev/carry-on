bb13899de52ea7136a4515bed75817c4fd908c73
Russ Panula <russ@dfa.net>
2021-06-13 02:34:22 -0400

    extract nodesToProps

--- packages/carry-on-react/src/withNodesToProps.js
+++ packages/carry-on-react/src/withNodesToProps.js
@@ -2,47 +2,54 @@
 import React from "react";
 import hoistNonReactStatic from "hoist-non-react-statics";
 
+const nodesToProps = children => {
+  const childs = Array.isArray(children) ? children : [children];
+
+  const newProps = {};
+
+  for (let i = 0, len = childs.length; i < len; i += 1) {
+    const child = childs[i];
+    if (child === undefined || child === null) continue;
+
+    // fetch node map config
+    const name = (child.type && child.type.name) || "nodesToProp";
+    const {
+      prop = name.charAt(0).toLowerCase() + name.slice(1),
+      val = "children",
+      transform,
+      default: def
+    } = child.type || {};
+
+    // retrieve value
+    let v = child.props && child.props[val];
+
+    // default value
+    if (v === undefined) v = def;
+
+    // transform value
+    v = transform ? transform(v) : v;
+
+    // present as array when multiple nodes
+    const curProp = newProps[prop];
+    if (curProp !== undefined) {
+      if (Array.isArray(curProp)) {
+        curProp.push(v);
+      } else {
+        newProps[prop] = [curProp, v];
+      }
+    } else newProps[prop] = v;
+  }
+
+  return newProps;
+};
+
 const withNodesToProps = WrappedComponent => {
-  const WithNodesToProps = ({ children, ...props }) => {
-    if (!Array.isArray(children))
-      return <WrappedComponent {...props}>{children}</WrappedComponent>;
-
-    const newProps = {};
-
-    for (let i = 0, len = children.length; i < len; i += 1) {
-      const child = children[i];
-
-      // fetch node map config
-      const name = (child.type && child.type.name) || "nodesToProp";
-      const {
-        prop = name.charAt(0).toLowerCase() + name.slice(1),
-        val = "children",
-        transform,
-        default: def
-      } = child.type || {};
-
-      // retrieve value
-      let v = child.props[val];
-
-      // default value
-      if (v === undefined) v = def;
-
-      // transform value
-      v = transform ? transform(v) : v;
-
-      // present as array when multiple nodes
-      const curProp = newProps[prop];
-      if (curProp !== undefined) {
-        if (Array.isArray(curProp)) {
-          curProp.push(v);
-        } else {
-          newProps[prop] = [curProp, v];
-        }
-      } else newProps[prop] = v;
-    }
-
-    return <WrappedComponent {...props} {...newProps} />;
-  };
+  const WithNodesToProps = ({ children, ...props }) =>
+    !Array.isArray(children) && typeof children === "function" ? (
+      <WrappedComponent {...props}>{children}</WrappedComponent>
+    ) : (
+      <WrappedComponent {...props} {...nodesToProps(children)} />
+    );
 
   WithNodesToProps.displayName = "withNodesToProps";
   WithNodesToProps.WrappedComponent = WrappedComponent;
