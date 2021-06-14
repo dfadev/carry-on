3d30274b75c7042f869446f9079c5dd8b2d98d51
Russ Panula <russ@dfa.net>
2021-06-14 00:44:52 -0400

    fix should be able to coexist with @babel/runtime

--- buildHelper.js
+++ buildHelper.js
@@ -33,7 +33,7 @@ export default pkg => [
     external: [
       ...Object.keys(pkg.peerDependencies || {}),
       ...Object.keys(pkg.dependencies || {}),
-      /@babel\/runtime/
+      /@babel\/runtime-corejs3/
     ]
   }
 ];
