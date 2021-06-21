57e057b292769fd718d803d7af3f9a5a73719c94
Russ Panula <russ@dfa.net>
2021-06-21 12:12:16 -0400

    use separate environments, use specific browser targets

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,33 @@
+const common = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        modules: false,
+        useBuiltIns: "usage",
+        corejs: 3,
+        targets: {
+          chrome: "89",
+          edge: "89",
+          firefox: "87",
+          //ie: "11",
+          ios: "14",
+          safari: "14",
+          samsung: "13"
+        }
+        //debug: true
+      }
+    ]
+  ]
+};
+
 module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
-  plugins: [["@babel/plugin-transform-runtime", { corejs: 3 }]]
+  plugins: [["@babel/plugin-transform-runtime", { corejs: 3 }]],
+
+  env: {
+    production: { ...common },
+    development: { ...common },
+    test: {}
+  }
 };
