04f6cb8924655fa4946a84288b3f3c4464393a0b
Russ Panula <russ@dfa.net>
2021-06-21 12:09:45 -0400

    fix dependency bundling, add rollup visualizer

--- buildHelper.js
+++ buildHelper.js
@@ -3,37 +3,48 @@ import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
 import eslint from "@rollup/plugin-eslint";
 import camelcase from "camelcase";
+import { visualizer } from "rollup-plugin-visualizer";
 
-export default pkg => [
-  {
-    input: pkg.source,
-    output: [
-      {
-        name: camelcase(pkg.name),
-        file: pkg.browser,
-        format: "umd",
-        sourcemap: true
-      },
-      { file: pkg.main, format: "cjs", sourcemap: true },
-      { file: pkg.module, format: "es", sourcemap: true }
-    ],
-    plugins: [
-      resolve(),
-      eslint(),
-      commonjs({
-        include: /node_modules/,
-        sourcemap: false
-      }),
-      babel({
-        rootMode: "upward",
-        babelHelpers: "runtime"
-      })
-    ],
-    watch: { clearScreen: false },
-    external: [
-      ...Object.keys(pkg.peerDependencies || {}),
-      ...Object.keys(pkg.dependencies || {}),
-      /@babel\/runtime-corejs3/
-    ]
-  }
-];
+const replace = item =>
+  item.indexOf("@") > -1
+    ? new RegExp(item.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&"))
+    : item;
+
+export default pkg => {
+  const external = [
+    ...Object.keys(pkg.peerDependencies || {}).map(replace),
+    ...Object.keys(pkg.dependencies || {}).map(replace)
+  ];
+
+  console.log("External:\n", external);
+  return [
+    {
+      input: pkg.source,
+      output: [
+        {
+          name: camelcase(pkg.name),
+          file: pkg.browser,
+          format: "umd",
+          sourcemap: true
+        },
+        { file: pkg.main, format: "cjs", sourcemap: true },
+        { file: pkg.module, format: "es", sourcemap: true }
+      ],
+      plugins: [
+        resolve(),
+        eslint(),
+        commonjs({
+          include: /node_modules/,
+          sourcemap: false
+        }),
+        babel({
+          rootMode: "upward",
+          babelHelpers: "runtime"
+        }),
+        visualizer()
+      ],
+      watch: { clearScreen: false },
+      external
+    }
+  ];
+};
