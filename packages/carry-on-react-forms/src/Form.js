dcb61fb1ca590fcabd2f8fbf75053a3d2bd2e624
Russ Panula <russ@dfa.net>
2021-06-26 01:53:54 -0400

    use store from context if available

--- packages/carry-on-react-forms/src/Form.js
+++ packages/carry-on-react-forms/src/Form.js
@@ -1,53 +1,55 @@
 import React from "react";
-import { State, withNodesToProps } from "carry-on-react";
+import { withStore, State, withNodesToProps } from "carry-on-react";
 import FormContext from "./FormContext";
 import plugin from "./plugin";
 
-const Form = ({
-  from,
-  store = from,
-  id = "form",
-  children,
-  register = [],
-  // plugin
-  initialValues,
-  onValidate,
-  onSubmit,
-  onReset,
-  // State
-  debug,
-  verbose,
-  onMount,
-  onUnmount,
-  noFormTag
-}) => (
-  <FormContext.Provider value={{ store, form: id }}>
-    <State
-      path={id}
-      from={store}
-      register={[]
-        .concat(register)
-        .concat(plugin({ id, initialValues, onValidate, onSubmit, onReset }))}
-      debug={debug}
-      verbose={verbose}
-      onMount={onMount}
-      onUnmount={onUnmount}
-    >
-      {(form, { id: storeId } = {}) =>
-        noFormTag ? (
-          children
-        ) : (
-          <form
-            id={`${storeId ? `${storeId}.` : ""}${id}`}
-            onSubmit={form.submit}
-            onReset={form.reset}
-          >
-            {children}
-          </form>
-        )
-      }
-    </State>
-  </FormContext.Provider>
+const Form = withStore(
+  ({
+    from,
+    store = from,
+    id = "form",
+    children,
+    register = [],
+    // plugin
+    initialValues,
+    onValidate,
+    onSubmit,
+    onReset,
+    // State
+    debug,
+    verbose,
+    onMount,
+    onUnmount,
+    noFormTag
+  }) => (
+    <FormContext.Provider value={{ store, form: id }}>
+      <State
+        path={id}
+        from={store}
+        register={[]
+          .concat(register)
+          .concat(plugin({ id, initialValues, onValidate, onSubmit, onReset }))}
+        debug={debug}
+        verbose={verbose}
+        onMount={onMount}
+        onUnmount={onUnmount}
+      >
+        {(form, { id: storeId } = {}) =>
+          noFormTag ? (
+            children
+          ) : (
+            <form
+              id={`${storeId ? `${storeId}.` : ""}${id}`}
+              onSubmit={form.submit}
+              onReset={form.reset}
+            >
+              {children}
+            </form>
+          )
+        }
+      </State>
+    </FormContext.Provider>
+  )
 );
 
 Form.composes = [
