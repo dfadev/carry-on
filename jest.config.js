0b5373b6125b7b732ba1d4ee13fdf791c72eef15
Russ Panula <russ@dfa.net>
2021-06-30 04:33:17 -0400

    add material view and app to build scripts

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,28 @@
 /* eslint-disable no-restricted-syntax */
 const { guessRootConfig } = require("lerna-jest");
+
 const config = guessRootConfig(__dirname);
-for (let c of config.projects) {
-  if (c.name.indexOf("react") !== -1)
-    c.moduleNameMapper = {
-      "^react($|/.+)": "<rootDir>/node_modules/react$1",
-      "^react-dom($|/.+)": "<rootDir>/node_modules/react-dom$1"
-    };
+
+const reactProjects = [
+  "carry-on-react",
+  "carry-on-react-forms",
+  "carry-on-react-router",
+  "carry-on-react-view",
+  "carry-on-material-view",
+  "carry-on-material-app"
+];
+
+const reactProject = {
+  moduleNameMapper: {
+    "^react($|/.+)": "<rootDir>/node_modules/react$1",
+    "^react-dom($|/.+)": "<rootDir>/node_modules/react-dom$1"
+  }
+};
+
+for (const entry of config.projects) {
+  const nm = entry.name.slice(0, -12);
+  if (reactProjects.includes(nm)) Object.assign(entry, reactProject);
 }
 
 config.collectCoverageFrom = ["**/src/*.js"];
-
 module.exports = config;
