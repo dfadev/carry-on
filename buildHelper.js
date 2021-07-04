1ae14176386c48d611d9f841bcc8e22c70bd39b1
Russ Panula <russ@dfa.net>
2021-07-04 00:49:50 -0400

    use rollup-plugin-node-externals

--- buildHelper.js
+++ buildHelper.js
@@ -4,47 +4,35 @@ import babel from "@rollup/plugin-babel";
 import eslint from "@rollup/plugin-eslint";
 import camelcase from "camelcase";
 import { visualizer } from "rollup-plugin-visualizer";
+import externals from "rollup-plugin-node-externals";
 
-const replace = item =>
-  item.indexOf("@") > -1
-    ? new RegExp(item.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&"))
-    : item;
-
-export default pkg => {
-  const external = [
-    ...Object.keys(pkg.peerDependencies || {}).map(replace),
-    ...Object.keys(pkg.dependencies || {}).map(replace)
-  ];
-
-  console.log("External:\n", external);
-  return [
-    {
-      input: pkg.source,
-      output: [
-        {
-          name: camelcase(pkg.name),
-          file: pkg.browser,
-          format: "umd",
-          sourcemap: true
-        },
-        { file: pkg.main, format: "cjs", sourcemap: true },
-        { file: pkg.module, format: "es", sourcemap: true }
-      ],
-      plugins: [
-        resolve(),
-        eslint(),
-        commonjs({
-          include: /node_modules/,
-          sourcemap: false
-        }),
-        babel({
-          rootMode: "upward",
-          babelHelpers: "runtime"
-        }),
-        visualizer()
-      ],
-      watch: { clearScreen: false },
-      external
-    }
-  ];
-};
+export default pkg => [
+  {
+    input: pkg.source,
+    output: [
+      {
+        name: camelcase(pkg.name),
+        file: pkg.browser,
+        format: "umd",
+        sourcemap: true
+      },
+      { file: pkg.main, format: "cjs", sourcemap: true },
+      { file: pkg.module, format: "es", sourcemap: true }
+    ],
+    plugins: [
+      externals({ deps: true }),
+      resolve(),
+      eslint(),
+      commonjs({
+        include: /node_modules/,
+        sourcemap: false
+      }),
+      babel({
+        rootMode: "upward",
+        babelHelpers: "runtime"
+      }),
+      visualizer()
+    ],
+    watch: { clearScreen: false }
+  }
+];
