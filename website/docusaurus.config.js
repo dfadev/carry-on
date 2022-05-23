9338e6ae87959caff85e04c367007783935b0792
Russ Panula <russ@dfa.net>
2022-05-23 01:47:47 -0400

    add docusaurus-plugin-module-alias back to avoid duplicate react's

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -29,6 +29,94 @@ module.exports = {
       }
     ]
   ],
+  plugins: [
+    [
+      "docusaurus-plugin-module-alias",
+      {
+        alias: {
+          react: path.resolve(__dirname, "node_modules/react"),
+          "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
+          "object-assign": path.resolve(
+            __dirname,
+            "node_modules/object-assign"
+          ),
+          "prop-types": path.resolve(__dirname, "node_modules/prop-types"),
+          scheduler: path.resolve(__dirname, "node_modules/scheduler"),
+          "@babel/runtime": path.resolve(
+            __dirname,
+            "node_modules/@babel/runtime"
+          ),
+          //"@material-ui/core": path.resolve(
+            //__dirname,
+            //"node_modules/@material-ui/core"
+          //),
+          //"@material-ui/icons": path.resolve(
+            //__dirname,
+            //"node_modules/@material-ui/icons"
+          //),
+          //"@material-ui/styles": path.resolve(
+            //__dirname,
+            //"node_modules/@material-ui/styles"
+          //),
+          //"@material-ui/system": path.resolve(
+            //__dirname,
+            //"node_modules/@material-ui/system"
+          //),
+          //"@material-ui/utils": path.resolve(
+            //__dirname,
+            //"node_modules/@material-ui/utils"
+          //),
+          clsx: path.resolve(__dirname, "node_modules/clsx"),
+          "dom-helpers": path.resolve(__dirname, "node_modules/dom-helpers"),
+          "hoist-non-react-statics": path.resolve(
+            __dirname,
+            "node_modules/hoist-non-react-statics"
+          ),
+          "is-in-browser": path.resolve(
+            __dirname,
+            "node_modules/is-in-browser"
+          ),
+          jss: path.resolve(__dirname, "node_modules/jss"),
+          "react-is": path.resolve(__dirname, "node_modules/react-is"),
+          "react-transition-group": path.resolve(
+            __dirname,
+            "node_modules/react-transition-group"
+          ),
+          "tiny-warning": path.resolve(__dirname, "node_modules/tiny-warning"),
+          "@babel/runtime-corejs3": path.resolve(
+            __dirname,
+            "node_modules/@babel/runtime-corejs3"
+          ),
+          //"carry-on-react": path.resolve(
+          //__dirname,
+          //"node_modules/carry-on-react"
+          //),
+          //"carry-on-store": path.resolve(
+          //__dirname,
+          //"node_modules/carry-on-store"
+          //),
+          //"carry-on-utils": path.resolve(
+          //__dirname,
+          //"node_modules/carry-on-utils"
+          //),
+          "core-js-pure": path.resolve(__dirname, "node_modules/core-js-pure"),
+          "debounce-promise": path.resolve(
+            __dirname,
+            "../packages/carry-on-utils/node_modules/debounce-promise"
+          ),
+          "react-fast-compare": path.resolve(
+            __dirname,
+            "../packages/carry-on-utils/node_modules/react-fast-compare"
+          ),
+          "throttle-debounce": path.resolve(
+            __dirname,
+            "../packages/carry-on-utils/node_modules/throttle-debounce"
+          ),
+          rifm: path.resolve(__dirname, "node_modules/rifm")
+        }
+      }
+    ]
+  ],
   themes: ["@docusaurus/theme-live-codeblock"],
   themeConfig: {
     colorMode: {
